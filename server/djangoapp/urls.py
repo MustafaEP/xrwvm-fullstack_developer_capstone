43e93a1dafb700fe3176c191c5556c679d562108
MustafaEP <portakalm11@gmail.com>
2026-01-05 14:08:29 -0500

    Added CarMake and CarModel with admin and get_cars endpoint

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
     # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
 
+    path('get_cars', views.get_cars, name='getcars'),
+
     # path for dealer reviews view
     
 
